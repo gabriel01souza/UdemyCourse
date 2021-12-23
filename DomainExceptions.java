b2540ce7b1072a8bb109fba987b6df693a958531
gabriel01souza <88974782+gabriel01souza@users.noreply.github.com>
2021-12-23 14:48:54 -0300

    Add files via upload

---
+++ DomainExceptions.java
@@ -0,0 +1,19 @@
+package model.exceptions;
+
+public class DomainExceptions extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public DomainExceptions(String msg){
+        super(msg);
+    }
+
+
+
+
+
+
+
+
+
+
+}
